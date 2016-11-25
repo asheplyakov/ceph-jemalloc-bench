3023bac69a6df3000593bf6dbc7180c2e1f64d73
Alexey Sheplyakov <asheplyakov@mirantis.com>
2016-11-25 18:00:54 +0000

    Added host fio-rbd benchmark with writeback throttling disabled
    Makes maximal latency and its standard deviation somewhat bigger
    and does not improve throughput either.
    Therefore disabling filestore_wbthrottle_enable does not make sense.

---
+++ fio-host-nowbthrottling/runfio.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+IMAGE="rbd/`hostname`-fio-vol00.img"
+IMG_SIZE=512 # GB
+DURATION=60 # minutes
+
+# transparent hugepages and merging same pages cause major IOPS jitter
+sudo /bin/sh -c 'echo madvise > /sys/kernel/mm/transparent_hugepage/enabled'
+sudo /bin/sh -c 'echo 0 > /sys/kernel/mm/ksm/run'
+
+if rbd info "$IMAGE" >/dev/null 2>/dev/null; then
+	rbd rm "$IMAGE"
+fi
+
+rbd create \
+	--image-format 2 \
+	--image-feature layering \
+	--size ${IMG_SIZE}G \
+	"$IMAGE"
+
+cat > rbd-host.fio <<-EOF
+[global]
+write_bw_log=rbd-host-bw.log
+write_iops_log=rbd-host-iops.log
+ioengine=rbd
+rw=randwrite
+bs=4k
+clientname=admin
+pool=${IMAGE%%/*}
+invalidate=0
+runtime=$((DURATION*60))
+
+[rbd_iodepth32]
+rbdname=${IMAGE##*/}
+iodepth=32
+EOF
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.1 fio rbd-host.fio
