9bdb18228d20e9dab4496595e6789710faf8993e
Alexey Sheplyakov <asheplyakov@mirantis.com>
2016-11-25 12:04:35 +0000

    Added fio in quest with librbd benchmark and results

---
+++ rbd-guest/runfio.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+IMAGE="rbd/`hostname`-fio-vol00.img"
+IMG_SIZE=512 # GB
+DURATION=60 # minutes
+
+# transparent hugepages and merging same pages cause major IOPS jitter
+sudo /bin/sh -c 'echo madvise > /sys/kernel/mm/transparent_hugepage/enabled'
+sudo /bin/sh -c 'echo 0 > /sys/kernel/mm/ksm/run'
+
+rbd create \
+	--image-format 2 \
+	--image-feature layering \
+	--size ${IMG_SIZE}G \
+	"$IMAGE"
+
+cat > rbd-guest.fio <<-EOF
+[global]
+write_bw_log=rbd-guest-bw.log
+write_iops_log=rbd-guest-iops.log
+ioengine=rbd
+rw=randwrite
+bs=4k
+clientname=admin
+pool=${IMAGE%%/*}
+invalidate=0
+runtime=$((DURATION*60))
+
+[rbd_iodepth32]
+rbdname=${IMAGE##*/}
+iodepth=32
+EOF
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.1 fio rbd-guest.fio
